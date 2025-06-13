6315ce2f97af559e1f96d3f28f92ef08d01b2cf2
hang666 <ppmm@163.com>
2025-06-13 12:07:17 +0800

    version 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "just.trust.me.pro"
         minSdk = 21
         targetSdk = 34
-        versionCode = 10002
-        versionName = "1.0.2"
+        versionCode = 10003
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -22,4 +22,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "JustTrustMePro"
 include(":app")
- 
